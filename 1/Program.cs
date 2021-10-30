d408f8f5c1633b2606606b589f720747c525025a
YuLiaKhar <julia.harchenko02@gmail.com>
2021-10-30 18:03:30 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            Random random = new Random();
+            int sum=0;
+            int max =0;
+            int find;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+                Console.WriteLine(array[i]);
+                sum += array[i];
+                find = array[i];
+                if (find > max)
+                {
+                    max = find;
+                }
+            }
+
+            Task taskSum = new Task(() => Console.WriteLine($"Сумма всех чисел массива равна: {sum}")) ;
+            Task taskMax = new Task(() => Console.WriteLine($"Максимальное число равно: {max}")) ;
+            taskSum.Start();
+            taskMax.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
